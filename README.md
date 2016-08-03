2aa95b960da31f9b9cfe7e1f02582a595e091ce0
Jefree Sujit <JosephJ@advisory.com>
2016-08-04 00:00:35 +0530

    Created a basic Bot

--- README.md
+++ README.md
@@ -8,61 +8,73 @@ based on object-oriented principles.
 
 Additions welcome.
 
-# Current implementation
-Our current implementaion covers most of the basics of gameplay. The following methods are availible:
+## Current implementation
+This Bot focuses on collecting pokestops and hatching eggs automatically. 
+This bot is not made for catching pokemons, cuz it may lead to soft ban/hard ban of your pokemon go account.
+
+The bot is capable of:
+
+    * Getting your Profile details
+
+    * Getting your Inventory details
+
+    * Cleaning up your Inventory (non-important items)
+
+    * Mock your Location 
+
+    * Collect XP and Items from Pokestops
+
+    *  Incubate Eggs Automatically
+
+    [x]  Auto assign Eggs into empty Incubators
+
+
+## Installation
+
+### Step One: Install Prerequisites
+
+1. [Install Python 2.7+](https://wiki.python.org/moin/BeginnersGuide/Download)
+1. [Install PIP](https://pip.pypa.io/en/stable/installing/)
+
+(Latest versions of python comes along with pip)
+
+### Step Two: Install Pokemon_Go_API
+
+1. Download or clone the repository.
+1. Using a terminal, navigate into the cloned repository.
+1. Install all requirements for the project using 
+
+    `pip install -r requirements.txt`
+
+### Step Three: Run Pokemon_Go_API
+
+To run the project using Google login:
+
+```bash
+python main.py -a "google" -u "yourusername" -p "yourpassword123"  -l "Some Location, Some City"
+```
+
+To run the project using  Pokemon Trainer Club login:
+
+```bash
+python main.py -a "ptc" -u "yourusername" -p "yourpassword123" -l "Some Location, Some City"
+```
+
+### Authentication Note
+
+If you're authenticating with Google and have 2 factor authentication enabled for you account, you should
+create an [application specific password](https://support.google.com/accounts/answer/185833?hl=en).
+
+
+## Bot Example
+`main.py` includes an automated Bot script made out of the API.
 
 ```
-# Get profile
-def getProfile(self):
 
-# Get Location
-def getMapObjects(self, radius=10):
-
-# Spin a pokestop
-def getFortSearch(self, fort):
-
-# Get encounter
-def encounterPokemon(self, pokemon):
-
-# Upon Encounter, try and catch
-def catchPokemon(self, pokemon, pokeball=1):
-
-# Evolve Pokemon
-def evolvePokemon(self, pokemon):
-
-# Transfer Pokemon
-def releasePokemon(self, pokemon):
-
-# Throw away items
-def recycleItem(self, item_id, count):
-
-# set an Egg into an incubator
-def setEgg(self, item, pokemon):
-
-# Get Eggs
-def getEggs(self):
-
-# Get Inventory
-def getInventory(self):
-
-# Get Badges
-def getBadges(self):
-
-# Get Settings
-def getDownloadSettings(self):
-```
-Every method has been tested. Pull requests are encouraged.
-
-## Demo
-`demo.py` includes a demo of the API.
-
-```
-➜  python demo.py -a "google" -u "email@gmail.com" -p "thepassword" -l "The Atlantic Ocean"
-
-2016-07-17 16:26:59,947 - INFO - Creating Google session for email@gmail.com
+2016-07-17 16:26:59,947 - INFO - Creating Google session for someemail@gmail.com
 2016-07-17 16:26:59,953 - INFO - Starting new HTTPS connection (1): android.clients.google.com
 2016-07-17 16:27:00,362 - INFO - Starting new HTTPS connection (1): android.clients.google.com
-2016-07-17 16:27:00,789 - INFO - Location: The Atlantic Ocean
+2016-07-17 16:27:00,789 - INFO - Location: Your Location
 2016-07-17 16:27:00,789 - INFO - Coordinates: 51.01 7.12 0.0
 2016-07-17 16:27:00,793 - INFO - Starting new HTTPS connection (1): pgorelease.nianticlabs.com
 2016-07-17 16:27:01,633 - INFO - creation_time: 3341800000
@@ -87,30 +99,6 @@ currency {
 }
 ```
 
-This is achieved with minimal coding effort on the client's part
-(extract from `demo.py`):
-
-```
-  # ... Blabla define the parser
-  if args.auth == 'ptc':
-      session = api.createPTCSession(args.username, args.password, args.location)
-  elif args.auth == 'google':
-      session = api.createGoogleSession(args.username, args.password, args.location)
-
-  if session: # do stuff
-      profile = session.getProfile()
-      logging.info(profile)
-```
-
-## Contribution
-Hell yeah!
-I'm on [Slack](https://pkre.slack.com) too
-(want an [invite](https://shielded-earth-81203.herokuapp.com)?)
-if you want to have a quick chat.
-
-I welcome all PRs but for big changes it'd be best
-to open an issue so I have some idea of what's going on.
-This thing is under heavy development after all.
 
 ## Protocol
-We currently use [AeonLucid's Pokemon Go Protobuf protocol](https://github.com/AeonLucid/POGOProtos).
+This repo currently use [AeonLucid's Pokemon Go Protobuf protocol](https://github.com/AeonLucid/POGOProtos).
---
+++ main.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+import argparse
+import logging
+import time
+import sys
+
+sys.path.insert(0, 'pogo/')
+
+from custom_exceptions import GeneralPogoException
+
+from api import PokeAuthSession
+from location import Location
+
+from inventory import items
+
+def setupLogger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('Line %(lineno)d,%(filename)s - %(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+
+# Example functions
+# Get profile
+def getProfile(session):
+        logging.info("Printing Profile:")
+        profile = session.getProfile()
+        logging.info(profile)
+
+
+# Do Inventory stuff
+def getInventory(session):
+    logging.info("Get Inventory:")
+    logging.info(session.getInventory())
+
+
+# Basic solution to spinning all forts.
+# Since traveling salesman problem, not
+# true solution. But at least you get
+# those step in
+def sortCloseForts(session):
+    # Sort nearest forts (pokestop)
+    logging.info("Sorting Nearest Forts:")
+    cells = session.getMapObjects()
+    latitude, longitude, _ = session.getCoordinates()
+    ordered_forts = []
+    for cell in cells.map_cells:
+        for fort in cell.forts:
+            dist = Location.getDistance(
+                latitude,
+                longitude,
+                fort.latitude,
+                fort.longitude
+            )
+            if fort.type == 1:
+                ordered_forts.append({'distance': dist, 'fort': fort})
+
+    ordered_forts = sorted(ordered_forts, key=lambda k: k['distance'])
+    return [instance['fort'] for instance in ordered_forts]
+
+
+# Find the fort closest to user
+def findClosestForts(session):
+    # Find nearest fort (pokestop)
+    logging.info("Finding Nearest Fort:")
+    return sortCloseForts(session)
+
+
+# Walk to fort and spin
+def walkAndSpin(session, fort):
+    # No fort, demo == over
+    if fort:
+        details = session.getFortDetails(fort)
+        logging.info("Spinning the Fort \"%s\":" % details.name)
+
+        # Walk over
+        session.walkTo(fort.latitude, fort.longitude, step=3.2)
+        # Give it a spin
+        fortResponse = session.getFortSearch(fort)
+        logging.info(fortResponse)
+
+
+# Walk, spin pokestops and auto incubate eggs
+def walkSpinAndIncubate(session, forts):
+    inventory = session.checkInventory()
+    cleanInventory(session)
+    # Keep walking until you dont find pokestops
+    for fort in forts:
+        walkAndSpin(session, fort)
+        km_walk = inventory.stats.km_walked
+        trgt_km = inventory.incubators[0].target_km_walked
+        
+        #Displaying distance stats
+        logging.info("KM Walked : %f " % km_walk)
+        logging.info("Target KM : %f " % trgt_km)
+
+        if (km_walk >= trgt_km):
+            eggInfo = setEgg(session)
+            logging.info(eggInfo)
+
+
+# Set an egg to an incubator
+def setEgg(session):
+    inventory = session.checkInventory()
+    incubator = inventory.incubators[0]
+
+    # If no eggs, nothing we can do
+    if len(inventory.eggs) == 0:
+        return "NO EGGS"
+    
+    elif (incubator.target_km_walked != 0.0):
+        logging.info(incubator.target_km_walked)
+        return "INCUBATING"
+
+    egg = inventory.eggs[0]
+    return session.setEgg(incubator, egg)
+
+# Clean up all frequent drop items
+def cleanInventory(session):
+    logging.info("Cleaning out Inventory...")
+    bag = session.checkInventory().bag
+    
+    # Clear out all of a crtain type
+    tossable = [items.POTION, items.SUPER_POTION, items.REVIVE]
+    for toss in tossable:
+        if toss in bag and bag[toss]:
+            session.recycleItem(toss, bag[toss])
+
+    # Limit a certain type
+    limited = {
+        items.POKE_BALL: 50,
+        items.GREAT_BALL: 50,
+        items.ULTRA_BALL: 100,
+        items.RAZZ_BERRY: 25,
+        items.HYPER_POTION: 25
+    }
+    for limit in limited:
+        if limit in bag and bag[limit] > limited[limit]:
+            session.recycleItem(limit, bag[limit] - limited[limit])
+
+
+def initiatePokeBot(session):
+    # Trying not to flood the servers
+    cooldown = 1
+    
+    # Run the bot
+    while True:
+        forts = sortCloseForts(session)
+        cleanInventory(session)
+        try:
+            for fort in forts:
+                getInventory(session)
+                walkAndSpin(session, fort)
+                logging.info(session.checkInventory().stats.km_walked)
+                eggInfo = setEgg(session)
+                logging.info(eggInfo)
+
+                cooldown = 1
+                time.sleep(cooldown)
+
+        except Exception as e:
+            logging.critical('Exception raised: %s', e)
+            session = poko_session.reauthenticate(session)
+            time.sleep(cooldown)
+            cooldown *= 2
+
+
+
+# Entry point
+# Start off authentication and bot
+if __name__ == '__main__':
+    setupLogger()
+    logging.debug('Logger set up')
+
+    # Read in args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--auth", help="Auth Service", required=True)
+    parser.add_argument("-u", "--username", help="Username", required=True)
+    parser.add_argument("-p", "--password", help="Password", required=True)
+    parser.add_argument("-l", "--location", help="Location")
+    parser.add_argument("-g", "--geo_key", help="GEO API Secret")
+    args = parser.parse_args()
+
+    # Check service
+    if args.auth not in ['ptc', 'google']:
+        logging.error('Invalid auth service {}'.format(args.auth))
+        sys.exit(-1)
+
+    # Create PokoAuthObject
+    poko_session = PokeAuthSession(
+        args.username,
+        args.password,
+        args.auth,
+        geo_key=args.geo_key
+    )
+
+    # Authenticate with a given location
+    # Location is not inherent in authentication
+    # But is important to session
+    if args.location:
+        session = poko_session.authenticate(locationLookup=args.location)
+    else:
+        session = poko_session.authenticate()
+
+    # Time to show off what we can do
+    if session:
+
+        # General
+        getProfile(session)
+        getInventory(session)
+
+        # Things we need GPS for
+        if args.location:
+
+            # Pokestop, eggs related
+            initiatePokeBot(session)
+
+    else:
+        logging.critical('Session not created successfully')
